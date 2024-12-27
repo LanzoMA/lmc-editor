8e47e03b72d751855c87fa89abca326f66ccd69b
lanzoma <lanzo.ma9@gmail.com>
2024-12-27 09:58:44 +0000

    fixed ram display

--- src/components/RamContentsWindow.tsx
+++ src/components/RamContentsWindow.tsx
@@ -8,7 +8,7 @@ function RamContentsWindow(props: RamContentsWindowProps) {
     return <div className={`${styles.window} ${styles['output-window']}`}>
         RAM Contents
         <div className={styles['output-content']}>
-            {props.ram.map((value) => value === 0 ? '000' : value.toString()).join(' ')}
+            {props.ram.map((value) => value.toString().padStart(3, '0')).join(' ')}
         </div>
     </div>
 }

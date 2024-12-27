666747e492be6204f7c10df4e9c75e7043839435
lanzoma <lanzo.ma9@gmail.com>
2024-12-27 09:55:14 +0000

    renamed css style

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -2,7 +2,7 @@ import styles from '../styles/navigation.module.css'
 
 function NavigationBar() {
     return <nav>
-        <ul>
+        <ul className={styles['navigation-list']}>
             <li className={styles['navigation-item']}>About</li>
             <li className={styles['navigation-item']}>
                 <a href='https://www.vivaxsolutions.com/web/lmc.aspx'>Examples</a>
